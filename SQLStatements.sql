cca902fce74ce98c9b871bc4468651b89ea3111b
Parisa <134794618+parisa-0@users.noreply.github.com>
2024-03-02 17:13:10 +0000

    Update SQLStatements.sql

--- SQLStatements.sql
+++ SQLStatements.sql
@@ -8,3 +8,22 @@ CREATE TABLE Users (
 alter table Users
 ADD CONSTRAINT PK_un
 Primary KEY (Username) ;
+
+CREATE TABLE Movies (
+    MovieName varchar(255),
+    AverageRating varchar(255),
+    Review varchar(255),
+    YearAdded datetime
+);
+
+CREATE TABLE UserMoviesWatched (
+    UserName varchar(255),
+    MovieName varchar(255),
+    DateMovieWatched datetime
+);
+
+CREATE TABLE UserMoviesSaved (
+    UserName varchar(255),
+    MovieName varchar(255),
+    DateMovieWatched datetime
+);
